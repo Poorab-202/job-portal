6b13d2b7abb4044e857f1cbdc42fe08765059485
poorab patel <poorabpatel202@gmail.com>
2025-01-11 11:37:54 +0530

    connected mongodb using mongoose

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js"
+dotenv.config({});
 const app=express();
 
 
@@ -10,13 +13,15 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
     origin:'http//localhost:5173',
-    credentials:ture
+    credentials:true
 }
 app.use(cors(corsOptions));
 
 
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
+
 app.listen(PORT,()=>
 {
+    connectDB();
     console.log(`server running at port ${PORT}`);
 })
