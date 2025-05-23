2ab07e3dac5fdfac6d13648d5d58f819de9b2606
poorab patel <poorabpatel202@gmail.com>
2025-05-23 23:00:52 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,22 @@ import applicationRoutes from "./routes/application.route.js"
 
 dotenv.config({});
 const app = express();
+const allowedOrigins = [
+    'http://localhost:5173', 
+    'https://jobeasy-frontend.onrender.com' 
+];
 
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true 
+}));
 
 // middleware
 app.use(express.json());
